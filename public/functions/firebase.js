93bd6ab635f4cc3f979726858ccd8d38f436038a
Lucas Scognamiglio <lucas.scognamiglio@tero.us>
2023-07-27 20:26:48 -0300

    Agregue la configuracion de firebase

---
+++ public/functions/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBpWNyh3gJB_0ObrqKhzSQhuctGJKhy8zo",
+  authDomain: "better-uy.firebaseapp.com",
+  projectId: "better-uy",
+  storageBucket: "better-uy.appspot.com",
+  messagingSenderId: "148970078549",
+  appId: "1:148970078549:web:1cf447200b89c16962efdc"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
